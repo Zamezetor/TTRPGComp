53f7f837f41fdb8c5d70da3d6a65265723c95467
Logan White <149831319+Zamezetor@users.noreply.github.com>
2025-01-21 14:21:26 -0700

    GUI Start

--- rpgCompanion.py
+++ rpgCompanion.py
@@ -1,6 +1,15 @@
 # Imports
 import random
+import guizero as gz
 # Variables
 # Classes
 # Functions
 # Code
+app = gz.App(title="TTRPG Companion  ",width=1920, height=1080,layout = "grid",bg="#FFFFFF",visible=True)
+
+info_box = gz.Box(app, grid = [0,0],width=800,height=1080,border=1)
+initiative_box = gz.Box(app, grid = [800,0],width=960,height=1080,border=1)
+options_box = gz.Box(app, grid = [1760,0],width=240,height=1080,border=1)
+
+
+app.display()
