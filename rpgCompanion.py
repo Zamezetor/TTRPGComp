19f062f5f5506f0b58f9134c07a53c32f950a85e
Logan White <149831319+Zamezetor@users.noreply.github.com>
2025-01-23 14:16:57 -0700

    GUI Work and Icon Pack start

--- rpgCompanion.py
+++ rpgCompanion.py
@@ -5,11 +5,13 @@ import guizero as gz
 # Classes
 # Functions
 # Code
-app = gz.App(title="TTRPG Companion  ",width=1920, height=1080,layout = "grid",bg="#FFFFFF",visible=True)
+app = gz.App(title="TTRPG Companion  ",width=1920, height=1080,layout = "grid",bg="#6C6C6C",visible=True)
 
-info_box = gz.Box(app, grid = [0,0],width=800,height=1080,border=1)
-initiative_box = gz.Box(app, grid = [800,0],width=960,height=1080,border=1)
-options_box = gz.Box(app, grid = [1760,0],width=240,height=1080,border=1)
+info_box = gz.Box(app, grid = [0,0], width = 800, height = 1080, border = 1)
+initiative_box = gz.Box(app, grid = [800,0], width = 900, height = 1080, border = 1)
+options_box = gz.Box(app, grid = [1760,0], width = 220, height = 1080, border = 1)
+
+
 
 
 app.display()
