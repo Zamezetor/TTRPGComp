7a67c25331eb0c7c884100a61b143d0c41956b36
Logan White <149831319+Zamezetor@users.noreply.github.com>
2025-01-29 14:25:41 -0700

    update gui

--- rpgCompanion.py
+++ rpgCompanion.py
@@ -1,42 +1,79 @@
 # Imports
-import random
-import json
+import random, json, math, PIL
 import guizero as gz
+import tkinter as tk
+
+
 # Variables
+
 # Classes
+class mainApp():
+  def __init__(self,width,height):
+    self.width = width
+    self.height = height
+    self.running = True
+     
+    # Opens Settings
+    self.reminders_file = open("./datapacks/data/reminders.json", "r") 
+    self.datapack_file = open("./datapacks/data/default.json", "r")
+    self.settings_file = open("./settings.json", "r")
+    settings = json.load(self.settings_file)
+    print(settings)
+    if settings["dark-mode"]:
+      bg = "#6C6C6C"
+    else:
+      bg = "#ffffff"
+
+    self.app = gz.App(title = "TTRPG Companion", width = width, height = height, layout = "grid", bg = bg, visible = True)
+
+
+  def adjust_size(self):
+    self.width = self.app.width
+    self.height = self.app.height
+
+
+  def isRunning(self):
+    while self.running:
+      self.app.when_resized = self.adjust_size()
+      self.info_width = (self.width/3)
+      self.opts_width =  self.info_width/3
+      self.init_width = width - (self.info_width + self.opts_width)
+      
+
+      info_box = gz.Box(self.app, grid = [0, 0], width = self.info_width, height = height, layout = "grid")
+      init_box = gz.Box(self.app, grid = [1, 0], width = self.init_width, height = height, layout = "grid", border = 1, )
+      opts_box = gz.Box(self.app, grid = [2, 0], width = self.opts_width, height = height, layout = "grid")
+
+      time_box        = gz.Box(info_box, grid = [0, 0], width = self.info_width, height = self.info_width/4, layout = "grid")
+      icon_box_1      = gz.Box(info_box, grid = [0, 1], width = self.info_width, height = self.info_width/2, layout = "grid")
+      empty_box       = gz.Box(info_box, grid = [0, 2], width = self.info_width, height = (self.height-self.info_width-8), layout = "grid")
+      progression_box = gz.Box(info_box, grid = [0, 3], width = self.info_width, height = self.info_width/4 , layout = "grid")
+
+      weather_current = "clear.png"
+      seasons_current = "spring.png"
+      regions_current = "plains.png"
+      terrain_current = "flat.png"
+
+      weather_icon = gz.Picture(icon_box_1, grid = [0, 0], image = f"./datapacks/icons/weather/{weather_current}", width = int(self.info_width/2), height = int(self.info_width/4))
+      season_icon  = gz.Picture(icon_box_1, grid = [1, 0], image = f"./datapacks/icons/seasons/{seasons_current}", width = int(self.info_width/2), height = int(self.info_width/4))
+      region_icon  = gz.Picture(icon_box_1, grid = [0, 1], image = f"./datapacks/icons/regions/{regions_current}", width = int(self.info_width/2), height = int(self.info_width/4))
+      terrain_icon = gz.Picture(icon_box_1, grid = [1, 1], image = f"./datapacks/icons/terrain/{terrain_current}", width = int(self.info_width/2), height = int(self.info_width/4))
+
+      def showApp(self):
+        self.app.display()
+
 # Functions
+def getScreen(): 
+  # Spawn Tkinter window, take screen width and height, kill Tkinter window
+  root = tk.Tk()
+  width = root.winfo_screenwidth()
+  height = root.winfo_screenheight()
+  root.destroy()
+  return width, height
+
 # Code
-reminders_file = open("./datapacks/data/reminders.json", "r") 
-datapack_file = open("./datapacks/data/default.json", "r")
-settings_file = open("./settings.json", "r")
+width, height = getScreen()
+application = mainApp(width, height)
+application.showApp()
 
-
-
-app = gz.App(title = "TTRPG Companion", width = 1920, height = 1080, layout = "grid", bg = "#6C6C6C", visible = True)
-
-info_box = gz.Box(app, grid = [0,0], width = 800, height = 1080, border = 1, layout = "grid")
-init_box = gz.Box(app, grid = [800,0], width = 900, height = 1080, border = 1, layout = "grid")
-opts_box = gz.Box(app, grid = [1760,0], width = 220, height = 1080, border = 1, layout = "grid")
-
-
-
-time_box = gz.Box(info_box, grid = [0,0], align = "top", width = 800, height = 150, border = 1, layout = "grid")
-icon_box_1 = gz.Box(info_box, grid = [0,100], align = "top", width = 800, height = 400, border = 1, layout = "grid")
-
-weather_current = "clear.png"
-season_current = "spring.png"
-region_current = "plains.png"
-terrain_current = "flat.png"
-
-weather_icon = gz.Picture(icon_box_1, grid = [0, 0], image = f"./datapacks/icons/weather/{weather_current}", width = 400, height = 200)
-season_icon  = gz.Picture(icon_box_1, grid = [400, 0], image = f"./datapacks/icons/seasons/{season_current}", width = 400, height = 200)
-region_icon  = gz.Picture(icon_box_1, grid = [0, 200], image = f"./datapacks/icons/regions/{region_current}", width = 400, height = 200)
-terrain_icon = gz.Picture(icon_box_1, grid = [400, 200], image = f"./datapacks/icons/terrain/{terrain_current}", width = 400, height = 200)
-
-
-
-
-
-
-
-app.display()
+  
